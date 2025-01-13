20fb81724e9574e149f3e2e3592aeac2e4a75b29
Nixxy25 <tellaoyinkansola25@gmail.com>
2025-01-13 11:41:59 +0100

    eslint error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import HeroPage from "./pages/heropage";
 import BuiltPage from "./pages/builtpage";
 import AssemblePage from "./pages/assemble";
-import Card from "./pages/card";
+// import Card from "./pages/card";
 import HowItWorks from "./pages/howItworks";
 import FooterPage from "./pages/footerPage";
 
